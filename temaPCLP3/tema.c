9c7f16bc40e396baf0fdf209de3815ad273ab9de
darius-afk <darius_popescu28@yahoo.com>
2023-05-21 18:40:41 +0300

    hello

---
+++ temaPCLP3/tema.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void encryptFile(const char* inputFileName, const char* outputFileName, const char* key) {
+    
+}
+
+void decryptFile(const char* inputFileName, const char* outputFileName, const char* key) {
+    
+}
+
+int main() {
+    
+
+    return 0;
+}
