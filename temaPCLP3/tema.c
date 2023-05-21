f70c8e7fb09f4f89b69a4089c53dd4f472b1201b
darius-afk <darius_popescu28@yahoo.com>
2023-05-21 18:51:41 +0300

    a doua modificare

--- temaPCLP3/tema.c
+++ temaPCLP3/tema.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void encryptFile(const char* inputFileName, const char* outputFileName, const char* key) {
-    
+void encryptFile(const char* inputFileName, const char* outputFileName, const char* key) {// functia pentru criptare a fisierului primeste numele fisierului de intrare, de iesire si cheia
+    FILE *inputFile = fopen(inputFileName, "rt");// fisierele sunt de tip text
+    FILE *outputFile = fopen(outputFileName, "wt");
+
+    if (inputFile == NULL || outputFile == NULL) {// daca nu s-au putut deschide fisierele
+        printf("Eroare la deschiderea fisierelor!\n");
+        return;
+    }
+
+    fseek(inputFile, 0, SEEK_END);
+    long fileSize = ftell(inputFile); // returneaza pozitiaa curenta a indicatorului
+    rewind(inputFile); //seteaza pozitia curenta a indicatorului de fisier la inceputul fisierului
+
+    unsigned char* buffer = (unsigned char*)malloc(fileSize);// alocam memorie pentru buffer
+    fread(buffer, 1, fileSize, inputFile); // citim din fisier in buffer
+
 }
 
 void decryptFile(const char* inputFileName, const char* outputFileName, const char* key) {
